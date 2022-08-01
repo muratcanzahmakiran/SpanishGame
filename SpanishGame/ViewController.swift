812ab6d2dd66123a36770d5c721c13fba1622678
muratcanzahmakiran <muratcanzahmakiran@outlook.com>
2022-08-01 23:12:28 +0300

    Project is created.

---
+++ SpanishGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpanishGame
+//
+//  Created by Murat Can Zahmakıran on 1.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
